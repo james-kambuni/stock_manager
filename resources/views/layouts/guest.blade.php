13f30c415bce94504cdd67ba11a8b3ab1f9f7e00
james vundi <vkambuni@gmail.com>
2025-07-08 19:49:55 +0300

    login page background color

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,17 +15,19 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-[#f9f9f9]">
-        <div>
-            <!-- <a href="/">
-                <img src="{{ asset('images/junixlogo1.JPG') }}" alt="App Logo" class="w-20 h-20">
-            </a> -->
-        </div>
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+            <div>
+               <div>
+    <!-- <a href="/">
+        <img src="{{ asset('images/junixlogo1.JPG') }}" alt="App Logo" class="w-20 h-20">
+    </a> -->
+</div>
 
-        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-            {{ $slot }}
-        </div>
-    </div>
-</body>
+            </div>
 
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-green shadow-md overflow-hidden sm:rounded-lg">
+                {{ $slot }}
+            </div>
+        </div>
+    </body>
 </html>
